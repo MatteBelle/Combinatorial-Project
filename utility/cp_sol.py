f97f242f9d0e6acb07a9eab70d3666c5711b7506
CipStr <razvancipr.stricescu@studio.unibo.it>
2023-09-03 20:21:48 +0200

    fixed output and input of cp_graph and cp_sol

--- utility/cp_sol.py
+++ utility/cp_sol.py
@@ -1,3 +1,17 @@
+import sys
+
+to_split = sys.argv[1]
+# split the string into a list of strings, separated by -
+split = to_split.split('-')
+# get the number of couriers
+obj_val = split[0]
+# get the number of cuuriers
+m = split[1]
+# get the number of nodes
+n = split[2]
+# get the successor list
+successor = split[3:]
+
 def get_path(successor, n, m):
     paths_per_courier = [[] for _ in range(m)]
     for c in range(1, m+2):
@@ -7,10 +21,14 @@ def get_path(successor, n, m):
             x = successor[x-1]
     print(paths_per_courier)
 
-# Example usage:
-m = 3 # number of couriers
-n = 7
-successor = [6, 11, 7, 2, 4, 13, 12, 5, 3, 1, 9, 10, 8]
+# convert each string to an integer
+obj_val = int(obj_val)
+m = int(m)
+n = int(n)
+successor = successor[0]
+successor = successor.split('[')[1].split(']')[0].split(',')
+successor = [int(x) for x in successor]
+
 get_path(successor, n, m)
 
 
